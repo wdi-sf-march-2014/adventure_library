a64c98d6eda79a3f8aa374a7f97e7bec7a4fd91c
Raphael Sofaer <rafi@sofaer.net>
2014-02-14 12:54:12 -0800

    Update solution

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,6 +1,6 @@
 class AdventuresController < ApplicationController
   def index
     @adventures = Adventure.where(:library_id => nil)
-    render :json => {"adventures" => @adventures.as_json(:include => :pages)}
+    render :json => {"adventures" => @adventures.as_json(:include => :pages, :except => :id)}
   end
 end
