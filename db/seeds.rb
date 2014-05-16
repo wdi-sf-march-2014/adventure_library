f18686660e75ebab3f23472fa2ce60bc90354275
taylorchiu <taylorparkerchiu@gmail.com>
2014-05-15 17:14:15 -0700

    hide secrets and create models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+name = "Taylor"
 
 adv = Adventure.create!(:title => "#{name}'s Test Adventure",
 	:author => name)
