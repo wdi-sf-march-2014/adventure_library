e542f2a0135d6d403849dbcb85d140b953b4ccca
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-15 22:41:36 -0700

    Added seeds, routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+name = "Joshua P"
 
 adv = Adventure.create!(:title => "#{name}'s Test Adventure",
 	:author => name)
