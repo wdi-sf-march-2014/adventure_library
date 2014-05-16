498cc47309643ee6fab888e17bab35414c4c5be9
blairprophett <blaircuny@gmail.com>
2014-05-15 17:33:18 -0700

    added guid to adventure, updated adventure controller methods, added show and new views

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,2 +1,52 @@
 class AdventuresController < ApplicationController
+
+  def index
+    @adventures = Adventure.all
+  end
+
+  def show
+    @adventure = Adventure.find(params[:id])
+  end
+
+  def create
+    @adventure = Adventure.new(adventure_params)
+    @adventure.guid = SecureRandom.urlsafe_base64(10)
+    if @adventure.save
+      redirect_to adventures_path
+    else
+      flash[:errors] = @adventure.errors.full_messages
+      render :edit
+    end
+  end
+
+  def edit
+    @adventure = adventure.find(params[:id])
+  end
+
+  def update
+    @adventure = Adventure.new(adventure_params)
+    if @adventure.save
+      redirect_to adventure_path
+    else
+      flash[:errors] = @adventure.errors.full_messages
+      render :edit
+    end
+  end
+
+  def new
+    @adventure = Adventure.new
+  end
+
+  def destroy
+    @adventure = Adventure.find(params[:id])
+    @adventure.delete
+    redirect_to root_path
+  end
+
+  private
+    def adventure_params
+      params.require(:adventure).permit(:title, :author)
+    end
+
 end
+
