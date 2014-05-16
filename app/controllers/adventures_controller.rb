6dd63f4b28cf138a65d932638e41b8425c878724
mathur7 <mathur7@gmail.com>
2014-05-15 17:32:49 -0700

    added and tested randomcode generator for guid

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,2 +1,51 @@
 class AdventuresController < ApplicationController
+
+def index
+  @adventures = Adventure.all
+end
+
+def new
+  @adventure = Adventure.new
+end
+
+def create
+  @adventure = Adventure.new(adventure_params)
+  @adventure.GUID = SecureRandom.urlsafe_base64(10)
+  if @adventure.save
+    redirect_to adventures_path
+  else
+    flash[:errors] = @adventure.errors.full_messages
+    render :edit
+  end
+end
+
+def show
+  @adventure = Adventure.find(params[:id])
+end
+
+def edit
+  @adventure = Adventure.find(params[:id])
+end
+
+def update
+  @adventure = Adventure.find(params[:id])
+  if @adventure.update(adventure_params)
+    redirect_to root_path
+  else
+    flash[:errors] = @adventure.errors.full_messages
+    render :edit
+  end
+end
+
+def destroy
+  @adventure = Adventure.find(params[:id])
+  @adventure.delete
+  redirect_to root_path
+end
+
+private
+  def adventure_params
+    params.require(:adventure).permit(:title, :author)
+  end
+
 end
