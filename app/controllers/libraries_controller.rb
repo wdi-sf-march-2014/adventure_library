77b61d4995eec0579617d4d61106ef00f9803405
RahulKhanna <sociallyrk@gmail.com>
2014-05-16 16:54:18 -0700

    scrape the json

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,9 +1,7 @@
 class LibrariesController < ApplicationController
 
   def scrape_library(library)
-   binding.pry
    response = Typhoeus.get("#{library.url}/libraries.json")
-   response = Typhoeus.get("http://sleepy-garden-8077.herokuapp.com//libraries.json")
    result = JSON.parse(response.body)
    return result
   end
