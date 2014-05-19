56a9726bda15ab2a63e841dabcf424f3a1572a90
RahulKhanna <sociallyrk@gmail.com>
2014-05-19 09:30:05 -0700

    guid working

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,13 @@
 Library.create(:url => "localhost:3000")
 
 name = "Don Drapers"
-adv = Adventure.create!(:title => "#{name}'s Test Adventure",
-:author => name)
+adv = Adventure.create!(:title => "#{name}'s Test Adventure", :author => name, :guid => SecureRandom.urlsafe_base64(10))
 adv.pages.create!(:name => "start", :text => "This is a great text adventure which is beginning right now!  I can't wait to [[see Don Draper die!!!!|end]] or [[live!!!!|end2]]")
 adv.pages.create!(:name => "end", :text => "The show is over!")
 adv.pages.create!(:name => "end2", :text => "The show must go on!")
 
-adv = Adventure.create!(:title => "#Walter Whites's Coding Adventure",
-:author => name)
-adv.pages.create!(:name => "start", :text => "This is a great text adventure which is beginning right now!  I can't wait to [[see Heizenberg hack a hack!!!!|end]] or [[hackathon!!!!|end3]]")
-adv.pages.create!(:name => "end4", :text => "The show is over!")
-adv.pages.create!(:name => "end5", :text => "The show must go on!")
-
-adv = Adventure.create!(:title => "Rahul's Test Adventure", :author => "Rahul")
+adv2 = Adventure.create!(:title => "#Walter Whites's Coding Adventure",
+:author => name, :guid => SecureRandom.urlsafe_base64(10))
+adv2.pages.create!(:name => "start", :text => "This is a great text adventure which is beginning right now!  I can't wait to [[see Heizenberg hack a hack!!!!|end]] or [[hackathon!!!!|end3]]")
+adv2.pages.create!(:name => "end4", :text => "The show is over!")
+adv2.pages.create!(:name => "end5", :text => "The show must go on!")
